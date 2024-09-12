ea89d9c24338d31b07504370ec2477a27ba78ef7
SpiralP <spiral.palmer@gmail.com>
2024-09-12 11:56:26 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+
+      makePackages = (system: dev:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          inherit (lib.importTOML ./Cargo.toml) package;
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = package.name;
+            version = package.version;
+
+            src = lib.sourceByRegex ./. [
+              "^\.cargo(/.*)?$"
+              "^build\.rs$"
+              "^Cargo\.(lock|toml)$"
+              "^src(/.*)?$"
+            ];
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = { };
+            };
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              rustPlatform.bindgenHook
+            ] ++ (if dev then
+              with pkgs; [
+                clippy
+                rustfmt
+                rust-analyzer
+              ] else [ ]);
+          };
+        }
+      );
+    in
+    builtins.foldl' lib.recursiveUpdate { } (builtins.map
+      (system: {
+        devShells.${system} = makePackages system true;
+        packages.${system} = makePackages system false;
+      })
+      lib.systems.flakeExposed);
+}
